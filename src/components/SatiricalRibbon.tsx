504df5e9a2ca37b3f33943aaadef453aed1ad4e9
Milton Läufer <miltonlaufer@gmail.com>
2026-01-17 14:13:45 +0100

    fix: ribbon z-index

--- src/components/SatiricalRibbon.tsx
+++ src/components/SatiricalRibbon.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 
 export const SatiricalRibbon: React.FC = React.memo(function SatiricalRibbon() {
   return (
-    <div className="fixed top-3 -right-9 z-40 pointer-events-none">
+    <div className="fixed top-3 -right-9 z-[60] pointer-events-none">
       <div className="bg-[#d32f2f] text-white font-sans text-[9px] font-bold uppercase tracking-wider py-1 px-9 rotate-45 shadow-md">
         Satirical
       </div>
